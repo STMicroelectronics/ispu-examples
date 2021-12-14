b6537e89476ccca5baabce3bccb0b7b40399ebe5
Federico Rizzardini <federico.rizzardini@st.com>
2021-12-14 15:02:16 +0100

    align register map header to datasheet
    Signed-off-by: Federico Rizzardini <federico.rizzardini@st.com>

--- iis3is/norm/stred/src/main.c
+++ iis3is/norm/stred/src/main.c
@@ -14,17 +14,17 @@ void __attribute__ ((signal)) algo_00(void)
 	int16_t acc[3];
 	float norm;
 
-	acc[0] = cast_sint16_t(RL_ADDR_araw_x);
-	acc[1] = cast_sint16_t(RL_ADDR_araw_y);
-	acc[2] = cast_sint16_t(RL_ADDR_araw_z);
+	acc[0] = cast_sint16_t(STRED_ARAW_X);
+	acc[1] = cast_sint16_t(STRED_ARAW_Y);
+	acc[2] = cast_sint16_t(STRED_ARAW_Z);
 
 	norm = sqrtf(acc[0] * acc[0] + acc[1] * acc[1] + acc[2] * acc[2]);
 
-	cast_sint16_t(RL_ADDR_DOUT_00) = acc[0];
-	cast_sint16_t(RL_ADDR_DOUT_01) = acc[1];
-	cast_sint16_t(RL_ADDR_DOUT_02) = acc[2];
+	cast_sint16_t(STRED_DOUT_00) = acc[0];
+	cast_sint16_t(STRED_DOUT_01) = acc[1];
+	cast_sint16_t(STRED_DOUT_02) = acc[2];
 
-	cast_float(RL_ADDR_DOUT_03) = norm;
+	cast_float(STRED_DOUT_03) = norm;
 
 	int_status = int_status | 0x0001;
 }
@@ -32,28 +32,28 @@ void __attribute__ ((signal)) algo_00(void)
 int main(void)
 {
 	// set boot done flag
-	uint8_t RL_STATUS = cast_uint8_t(RL_ADDR_RL_STATUS);
-	RL_STATUS = RL_STATUS | 0x04;
-	cast_uint8_t(RL_ADDR_RL_STATUS) = RL_STATUS;
+	uint8_t status = cast_uint8_t(STRED_STATUS);
+	status = status | 0x04;
+	cast_uint8_t(STRED_STATUS) = status;
 
 	while (1) {
 		stop_and_wait_start_pulse;
 
 		// reset status registers and interrupts
 		int_status = 0;
-		cast_uint8_t(RL_ADDR_INT_STATUS) = 0x00;
-		cast_uint8_t(RL_ADDR_INT_PAD) = 0x00;
+		cast_uint8_t(STRED_INT_STATUS) = 0x00;
+		cast_uint8_t(STRED_INT_PAD) = 0x00;
 
 		// get all the algorithms to run in this time slot
-		cast_uint32_t(RL_ADDR_CALL_EN) = cast_uint32_t(RL_ADDR_ALGO) << 1;
+		cast_uint32_t(STRED_CALL_EN) = cast_uint32_t(STRED_ALGO) << 1;
 
 		// wait for all algorithms execution
-		while (cast_uint32_t(RL_ADDR_CALL_EN)) {}
+		while (cast_uint32_t(STRED_CALL_EN)) {}
 
 		// set status registers and generate interrupts
-		cast_uint32_t(RL_ADDR_INT_STATUS) = int_status;
-		cast_uint8_t(RL_ADDR_INT_PAD) = ((int_status & cast_uint32_t(RL_ADDR_INT1_CTRL) > 0) << 0) |
-			((int_status & cast_uint32_t(RL_ADDR_INT2_CTRL) > 0) << 1);
+		cast_uint32_t(STRED_INT_STATUS) = int_status;
+		cast_uint8_t(STRED_INT_PAD) = ((int_status & cast_uint32_t(STRED_INT1_CTRL) > 0) << 0) |
+			((int_status & cast_uint32_t(STRED_INT2_CTRL) > 0) << 1);
 	}
 }
 
--- iis3is/template/stred/src/main.c
+++ iis3is/template/stred/src/main.c
@@ -22,28 +22,28 @@ void __attribute__ ((signal)) algo_00(void)
 int main(void)
 {
 	// set boot done flag
-	uint8_t RL_STATUS = cast_uint8_t(RL_ADDR_RL_STATUS);
-	RL_STATUS = RL_STATUS | 0x04;
-	cast_uint8_t(RL_ADDR_RL_STATUS) = RL_STATUS;
+	uint8_t status = cast_uint8_t(STRED_STATUS);
+	status = status | 0x04;
+	cast_uint8_t(STRED_STATUS) = status;
 
 	while (1) {
 		stop_and_wait_start_pulse;
 
 		// reset status registers and interrupts
 		int_status = 0;
-		cast_uint8_t(RL_ADDR_INT_STATUS) = 0x00;
-		cast_uint8_t(RL_ADDR_INT_PAD) = 0x00;
+		cast_uint8_t(STRED_INT_STATUS) = 0x00;
+		cast_uint8_t(STRED_INT_PAD) = 0x00;
 
 		// get all the algorithms to run in this time slot
-		cast_uint32_t(RL_ADDR_CALL_EN) = cast_uint32_t(RL_ADDR_ALGO) << 1;
+		cast_uint32_t(STRED_CALL_EN) = cast_uint32_t(STRED_ALGO) << 1;
 
 		// wait for all algorithms execution
-		while (cast_uint32_t(RL_ADDR_CALL_EN)) {}
+		while (cast_uint32_t(STRED_CALL_EN)) {}
 
 		// set status registers and generate interrupts
-		cast_uint32_t(RL_ADDR_INT_STATUS) = int_status;
-		cast_uint8_t(RL_ADDR_INT_PAD) = ((int_status & cast_uint32_t(RL_ADDR_INT1_CTRL) > 0) << 0) |
-			((int_status & cast_uint32_t(RL_ADDR_INT2_CTRL) > 0) << 1);
+		cast_uint32_t(STRED_INT_STATUS) = int_status;
+		cast_uint8_t(STRED_INT_PAD) = ((int_status & cast_uint32_t(STRED_INT1_CTRL) > 0) << 0) |
+			((int_status & cast_uint32_t(STRED_INT2_CTRL) > 0) << 1);
 	}
 }
 
