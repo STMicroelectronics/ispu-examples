881c0a6993eefbc2cf4eb1082a38b4b57e3c7f80
Lorenzo Bracco <lorenzo.bracco@st.com>
2022-07-20 16:59:55 +0200

    add license
     - Add SLA0095 license files in the root
     - Add neutral license header to .h and .c files (excluding outputs and libs)
    
    Signed-off-by: Lorenzo Bracco <lorenzo.bracco@st.com>

--- ism330is_lsm6dso16is/crc32/ispu/inc/crc32.h
+++ ism330is_lsm6dso16is/crc32/ispu/inc/crc32.h
@@ -1,3 +1,17 @@
+/**
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
 #ifndef _CRC32_H_
 #define _CRC32_H_
 
