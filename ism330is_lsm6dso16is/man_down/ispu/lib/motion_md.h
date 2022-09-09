46e134a20eff5de17a8649794895bfb338b8b4df
Lorenzo Bracco <lorenzo.bracco@st.com>
2022-09-09 11:22:59 +0200

    fix license in libs APIs
    Signed-off-by: Lorenzo Bracco <lorenzo.bracco@st.com>

--- ism330is_lsm6dso16is/man_down/ispu/lib/motion_md.h
+++ ism330is_lsm6dso16is/man_down/ispu/lib/motion_md.h
@@ -1,40 +1,23 @@
 /**
- ******************************************************************************
+ *******************************************************************************
  * @file    motion_md.h
  * @author  MEMS Application Team
  * @version V1.0.0
  * @brief   Header for motion_md module
- ******************************************************************************
+ *******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2022 STMicroelectronics</center></h2>
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright notice,
- *      this list of conditions and the following disclaimer in the documentation
- *      and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its contributors
- *      may be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************
+ *******************************************************************************
  */
 
-/* Define to prevent recursive inclusion -------------------------------------*/
+/* Define to prevent recursive inclusion ------------------------------------ */
 #ifndef _MOTION_MD_H_
 #define _MOTION_MD_H_
 
@@ -43,7 +26,7 @@ extern "C"
 {
 #endif
 
-/* Includes ------------------------------------------------------------------*/
+/* Includes ----------------------------------------------------------------- */
 #include <stdint.h>
 
 /** @addtogroup MIDDLEWARES
@@ -54,7 +37,7 @@ extern "C"
  *  @{
  */
 
-/* Exported types ------------------------------------------------------------*/
+/* Exported types ----------------------------------------------------------- */
 typedef struct
 {
   float acc[3];  /* Accelerometer sensor output [g] */
@@ -81,9 +64,9 @@ typedef enum
  *  @}
  */
 
-/* Exported constants --------------------------------------------------------*/
-/* Exported variables --------------------------------------------------------*/
-/* Exported macro ------------------------------------------------------------*/
+/* Exported constants ------------------------------------------------------- */
+/* Exported variables ------------------------------------------------------- */
+/* Exported macro ----------------------------------------------------------- */
 
 /** @defgroup MOTION_MD_Exported_Functions MOTION_MD_Exported_Functions
  *  @{
@@ -143,6 +126,4 @@ uint8_t MotionMD_get_lib_version(char *version);
 #endif
 
 #endif /* _MOTION_MD_H_ */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
 
